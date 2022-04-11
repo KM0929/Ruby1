44676a1e4874f1ef8b228c070b8f01fbf5926a3c
村井紀元 <muraikigen@muraikigennoMacBook-Pro.local>
2022-04-11 23:56:17 +0900

    メモアプリの提出

---
+++ memo.rb
@@ -0,0 +1,40 @@
+require "csv"
+
+puts "1(新規でメモを作成) 2(既存のメモを編集する)"
+
+memo_type = gets.chomp.to_s
+
+if memo_type === "1"
+    puts "新規作成：拡張子を除いてファイル名を入力してください"
+    
+    file_name = gets.chomp.to_s
+    
+    puts "メモしたい内容を記入してください"
+    puts "完了したら　Ctrl+D　をおします"
+
+    content = gets.chomp.to_s
+
+    CSV.open("#{file_name}.csv","w") do |csv|
+        csv << [content]
+    end
+
+elsif memo_type === "2"
+    puts "編集：対象のファイル名を拡張子を除いて入力してください"
+    
+    file_name = gets.chomp.to_s
+    
+    puts "メモしたい内容を記入してください"
+    puts "完了したら　Ctrl+D　をおします"
+
+    content = gets.chomp.to_s
+    
+    CSV.open("#{file_name}.csv","a") do |csv|
+    csv << [content]
+    end
+
+else 
+    puts "１か２を入力してください"
+    puts "メモアプリを終了します"
+
+end
+
